1be2c81fd047ed7b7dbc51de8050774441242051
Chamin <cm.son@itbob.co.kr>
2023-03-30 15:13:02 +0900

    commit

--- lib/Watch_ReSearch.dart
+++ lib/Watch_ReSearch.dart
@@ -27,7 +27,6 @@ class _Watch_ReSurchState extends State<Watch_ReSurch> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  margin: const EdgeInsets.only(bottom: 10),
                   child: Text(
                     '다시 검색하기',
                     style: TextStyle(
@@ -38,6 +37,7 @@ class _Watch_ReSurchState extends State<Watch_ReSurch> {
                   ),
                 ),
                 Container(
+                  margin: const EdgeInsets.only(bottom: 10),
                   child: Image.asset(
                     'assets/splash_image/_splash_logo.png', width: 30,),
                 ),
