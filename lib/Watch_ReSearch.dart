0951cd5875a5808ca9270456f3ee4dfb19795ce0
Chamin <cm.son@itbob.co.kr>
2023-03-30 15:22:37 +0900

    commit

--- lib/Watch_ReSearch.dart
+++ lib/Watch_ReSearch.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 class Watch_ReSurch extends StatefulWidget {
   const Watch_ReSurch({Key? key}) : super(key: key);
 
@@ -13,35 +15,44 @@ class _Watch_ReSurchState extends State<Watch_ReSurch> {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.bottomCenter,
-      decoration: BoxDecoration(
-          gradient: const LinearGradient(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [Colors.transparent, Colors.black],
               stops: [.50, .75])),
-      child: Center(
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 15),
         child: Stack(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  child: Text(
-                    '다시 검색하기',
-                    style: TextStyle(
-                      color: Colors.lightBlueAccent,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 10),
+                  margin: const EdgeInsets.only(right: 10),
                   child: Image.asset(
-                    'assets/splash_image/_splash_logo.png', width: 30,),
+                    'assets/splash_image/_splash_logo.png', width: 20),
                 ),
-
+                TextButton(
+                    child:  const Text(
+                      '다시 검색하기',
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const TabPage()));
+                })
+                //
+                //
+                //
+                //
+                //
+                //
+                //
+                //
               ],
             ),
           ],
